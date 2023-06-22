7fa8846590c9c7c244713f9f3ef1473677750b35
Sumshi <sumayasomow@gmail.com>
2023-06-22 10:10:02 +0000

    mul opcode

--- mul.c
+++ mul.c
@@ -6,17 +6,16 @@
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
+	int result;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-		_free(*stack);
+		/*_free(*stack);*/
 		exit(EXIT_FAILURE);
 	}
 
-	temp = (*stack)->next;
-	temp->n *= (*stack)->n;/*multiplies second top by top element*/
-
+	result = ((*stack)->next->n) * ((*stack)->n);
 	pop(stack, line_number);/*removes top element from stack*/
+	(*stack)->n = result;
 }
