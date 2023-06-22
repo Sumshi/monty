638fdb69986800b4d5f00899d3acc7d45eef2c7b
Sumshi <sumayasomow@gmail.com>
2023-06-22 09:52:51 +0000

    sub opcode

--- sub.c
+++ sub.c
@@ -7,15 +7,15 @@
 
 void sub(stack_t **stack, unsigned int line_number)
 {
-	int result;
+	int sub;
 
-	if (!stack || !*stack || !((*stack)->next))
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		/*_free(*stack);*/
 		exit(EXIT_FAILURE);
 	}
-	result = ((*stack)->next->n) - ((*stack)->n);
+	sub = ((*stack)->next->n) - ((*stack)->n);
 	pop(stack, line_number);
-	(*stack)->n = result;
+	(*stack)->n = sub;
 }
